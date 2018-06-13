fe3ffd0d076a35698fdeb4ae987c556040f85382
pavanpodila <pavan.podila@gmail.com>
2018-06-13 20:50:53 +0530

    some stylistic improvements

--- src/core/app.js
+++ src/core/app.js
@@ -103,7 +103,6 @@ function ChapterRoute({ ex }) {
                         }}
                     >
                         <CardContent>
-                            {' '}
                             <Typography
                                 color={'textSecondary'}
                                 variant={'body2'}
